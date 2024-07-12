48871fc5d61eeaa901d5162e0333025ae398c04c
Mohit Gupta <135738292+mtg718@users.noreply.github.com>
2024-07-12 09:35:29 +0530

    Update contactFormRes.js

--- server/mail/templates/contactFormRes.js
+++ server/mail/templates/contactFormRes.js
@@ -74,7 +74,7 @@ exports.contactUsEmail = (
     
     <body>
         <div class="container">
-            <a href="https://skillsprint-edtech-project.vercel.app"><img class="logo"
+            <a href="https://skillsprint-edtech.vercel.app/"><img class="logo"
                     src="https://drive.google.com/uc?export=view&id=1vHVAN34Gm2Ei3AwIyvPcUhEXCvKYNHE0" alt="SkillSprint Logo"></a>
             <div class="message">Contact Form Confirmation</div>
             <div class="body">
